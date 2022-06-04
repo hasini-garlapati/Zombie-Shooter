24faf2874fa06cabecf13619bc4eaae964636601
hasini-garlapati <82131028+hasini-garlapati@users.noreply.github.com>
2022-06-04 09:57:56 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,145 @@
+var bg,bgImg;
+var gameOver, gameOverImg;
+var player, shooterImg, shooter_shooting;
+var zombie, zombieImg;
+
+var heart1, heart2, heart3;
+var heart1Img, heart2Img, heart3Img;
+
+var zombieGroup;
+
+var bullets = 70;
+
+var gameState = "fight"
+
+
+function preload(){
+  
+  heart1Img = loadImage("assets/heart_1.png")
+  heart2Img = loadImage("assets/heart_2.png")
+  heart3Img = loadImage("assets/heart_3.png")
+
+  shooterImg = loadImage("assets/shooter_2.png")
+  shooter_shooting = loadImage("assets/shooter_3.png")
+
+  zombieImg = loadImage("assets/zombie.png")
+
+  bgImg = loadImage("assets/bg.jpeg")
+  gameOverImg = loadImage("assets/gameOver.png")
+
+}
+
+function setup() {
+
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //adding the background image
+  bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
+bg.addImage(bgImg)
+bg.scale = 1.1
+  
+//creating the gameOver sprite
+gameOver = createSprite(700,250);
+gameOver.visible = false;
+
+
+//creating the player sprite
+player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
+ player.addImage(shooterImg)
+   player.scale = 0.3
+   player.debug = true
+   player.setCollider("rectangle",0,0,300,300)
+
+
+    bulletGroup = new Group()
+    zombieGroup = new Group()
+
+
+
+}
+
+function draw() {
+  background(0); 
+
+
+if(gameState === "fight"){
+
+  //moving the player up and down and making the game mobile compatible using touches
+if(keyDown("UP_ARROW")||touches.length>0){
+  player.y = player.y-30
+}
+if(keyDown("DOWN_ARROW")||touches.length>0){
+ player.y = player.y+30
+}
+
+
+//release bullets and change the image of shooter to shooting position when space is pressed
+if(keyWentDown("space")){
+  bullet = createSprite(displayWidth-1150,player.y-30,20,10)
+  bullet.velocityX = 20
+  
+  bulletGroup.add(bullet)
+  player.depth = bullet.depth
+  player.depth = player.depth+2
+  player.addImage(shooter_shooting)
+  
+}
+
+//player goes back to original standing image once we stop pressing the space bar
+else if(keyWentUp("space")){
+  player.addImage(shooterImg)
+}
+
+if(zombieGroup.isTouching(player)){
+  gameState = 0;
+}
+//displaying "You Lost!" 
+if (gameState === 0){
+  textSize(100);
+  text("You Lost ",400,400);
+  gameOver.addImage("gameOver.png",gameOverImg);
+  gameOver.visible = true;
+  zombieGroup.destroyEach();
+}
+
+
+if(zombieGroup.isTouching(bulletGroup)){
+  for(var i=0;i<zombieGroup.length;i++){     
+      
+   if(zombieGroup[i].isTouching(bulletGroup)){
+        zombieGroup[i].destroy()
+        bulletGroup.destroyEach()
+       
+        } 
+  
+  }
+}
+
+//calling the function to spawn zombies
+enemy();
+}
+
+drawSprites();
+
+}
+
+
+//creating function to spawn zombies
+function enemy(){
+  if(frameCount%50===0){
+
+    //giving random x and y positions for zombie to appear
+    zombie = createSprite(random(500,1100),random(100,500),40,40)
+
+    zombie.addImage(zombieImg)
+    zombie.scale = 0.15
+    zombie.velocityX = -3
+    zombie.debug= true
+    zombie.setCollider("rectangle",0,0,400,400)
+   
+    zombie.lifetime = 400
+   zombieGroup.add(zombie)
+  }
+
+}
